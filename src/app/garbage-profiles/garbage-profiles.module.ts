35213d6e43da9f935217af75efa837c1d06e1f26
tuzhongyi <tuzhongyi@howell.net.cn>
2023-02-17 16:47:31 +0800

    pipe

--- src/app/garbage-profiles/garbage-profiles.module.ts
+++ src/app/garbage-profiles/garbage-profiles.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { PIPE_Modules } from '../pipes/pipe.module';
 import { TableComponents } from './components/tables/tables.module';
 import { GarbageProfilesRoutingModule } from './garbage-profiles-routing.module';
 
 @NgModule({
-  declarations: [...TableComponents],
+  declarations: [...TableComponents, ...PIPE_Modules],
   imports: [
     GarbageProfilesRoutingModule,
     BrowserModule,
---
+++ src/app/pipes/pipe.module.ts
@@ -0,0 +1,4 @@
+import { DateTimePipe } from './date-time.pipe';
+import { KeepHtmlPipe } from './keep-html.pipe';
+
+export const PIPE_Modules = [DateTimePipe, KeepHtmlPipe];
