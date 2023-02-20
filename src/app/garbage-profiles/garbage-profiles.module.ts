3bb65e16917d1a25cd9111d07f4fb882ebde3749
tuzhongyi <tuzhongyi@howell.net.cn>
2023-02-20 14:07:32 +0800

    删除 BrowserModule 导致的嵌套导入

--- src/app/garbage-profiles/garbage-profiles.module.ts
+++ src/app/garbage-profiles/garbage-profiles.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { HowellModule } from '../common/howell.module';
 import { MaterialModule } from '../material.module';
 import { TableComponents } from './components/tables/tables.module';
@@ -11,7 +10,6 @@ import { GarbageProfilesRoutingModule } from './garbage-profiles-routing.module'
   declarations: [...TableComponents],
   imports: [
     GarbageProfilesRoutingModule,
-    BrowserModule,
     CommonModule,
     FormsModule,
     HowellModule,
