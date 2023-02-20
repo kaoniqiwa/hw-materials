8effd69976bdecb960047e838f168951cdc82b3a
tuzhongyi <tuzhongyi@howell.net.cn>
2023-02-20 16:47:51 +0800

    font

--- proxy.conf.ts
+++ proxy.conf.ts
@@ -9,22 +9,23 @@ const PROXY_CONFIG = [
     context: [
       '/howell/ver10/data_service/',
       '/api/howell/ver10/aiop_service/',
+      '/api/howell/ver10/garbage_profiles/',
       '/video/wsplayer/',
     ],
     // target: 'http://iebs.51hws.cn',
     // target: 'http://192.168.21.241:9000',
-    target: 'http://wechat.51hws.cn',
+    target: 'http://wechat.51hws.cn:8080',
     // target: 'http://192.168.21.122:8080',
     changeOrigin: true,
     secure: false,
   },
-  {
-    context: ['/api/howell/ver10/garbage_profiles/'],
-    // target: 'http://192.168.21.241:9000',
-    target: 'http://wechat.51hws.cn:9999',
-    changeOrigin: true,
-    secure: false,
-  },
+  // {
+  //   context: ['/api/howell/ver10/garbage_profiles/'],
+  //   // target: 'http://192.168.21.241:9000',
+  //   target: 'http://wechat.51hws.cn:9999',
+  //   changeOrigin: true,
+  //   secure: false,
+  // },
 ];
 
 module.exports = PROXY_CONFIG;
