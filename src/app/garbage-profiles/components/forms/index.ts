f5a6f2b7f0611f8f3d6b2a77cff68d1f429d68e5
tuzhongyi <248262952@qq.com>
2023-03-10 17:44:36 +0800

    pmx

--- src/app/garbage-profiles/components/forms/index.ts
+++ src/app/garbage-profiles/components/forms/index.ts
@@ -1,13 +1,13 @@
 import { GarbageProfileDetailsDynamicForm } from './garbage-profile-details-forms/garbage-profile-details-dynamic/garbage-profile-details-dynamic.component';
 import { GarbageProfileDetailsForm1 } from './garbage-profile-details-forms/garbage-profile-details-form1/garbage-profile-details-form1.component';
-import { GarbageProfileDetailsForm2Component } from './garbage-profile-details-forms/garbage-profile-details-form2/garbage-profile-details-form2.component';
+import { GarbageProfileDetailsForm2 } from './garbage-profile-details-forms/garbage-profile-details-form2/garbage-profile-details-form2.component';
 import { DetailsForm3Component } from './garbage-profile-details-forms/garbage-profile-details-form3/garbage-profile-details-form3.component';
 import { GarbageProfileDetailsForm4 } from './garbage-profile-details-forms/garbage-profile-details-form4/garbage-profile-details-form4.component';
 import { GarbageProfileDetailsForm5 } from './garbage-profile-details-forms/garbage-profile-details-form5/garbage-profile-details-form5.component';
 
 export const FormComponents = [
   GarbageProfileDetailsForm1,
-  GarbageProfileDetailsForm2Component,
+  GarbageProfileDetailsForm2,
   DetailsForm3Component,
   GarbageProfileDetailsForm4,
   GarbageProfileDetailsForm5,
